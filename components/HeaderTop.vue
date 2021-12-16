6a498c7f9669bb2e4b486c89c88a22a8b84118fc
Muhannad <muhannad.al.khudari@gmail.com>
2021-12-16 11:45:10 +0200

    feat: waiting count section

--- components/HeaderTop.vue
+++ components/HeaderTop.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="d-flex flex-row justify-content-between top-header darkBlueSlateBlue1">
             <LazySectionUser />
-            <div>Waiting</div>
+            <LazySectionWaitingCount />
         </div>
     </div>
 </template>
---
+++ components/SectionWaitingCount.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="d-flex flex-row darkBlueSlateBlue2 align-items-center px20">
+        <div class="mr-0 ml-3">
+            <img src="../assets/icons/arrow-right-white.svg" class="w18 arrow-icon"/>
+        </div>
+        <div class="mx-1 fw800">
+            {{waitingList.length}}
+        </div>
+        <div class="mr-3 ml-2 fw800">
+            {{messages.waitingList.ar}}
+        </div>
+    </div>
+</template>
+
+<script>
+    import * as data  from "../assets/data/db.json";
+    export default {
+        setup() {
+            const waitingList = data.waitingList;
+            const messages = data.messages;
+
+            return {
+                waitingList,
+                messages
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .w18 {
+        width: 18px;
+    }
+
+    .arrow-icon {
+        filter: invert(0.8) sepia(1) saturate(5) hue-rotate(180deg);
+    }
+</style>
