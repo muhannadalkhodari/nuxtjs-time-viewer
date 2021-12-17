022250d20188a940d7baf3a111ead6a1de166680
Muhannad <muhannad.al.khudari@gmail.com>
2021-12-17 18:23:05 +0200

    feat: broadcast page

--- pages/expert-broadcast.vue
+++ pages/expert-broadcast.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
+    <div class="row m-0 g-sm-1 g-md-3" dir="ltr">
+
+        <div class="col-sm-12 col-md-7">
+            <LazyComponentItemViewer :item="selectedItem" />
+        </div>
+
+        <div class="col-sm-12 col-md-5 mt-5" dir="rtl">
+            <div class="row px-2">
+                <div
+                    v-for="item in doctor.products.filter(i => i.type === 'video')"
+                    :key="item"
+                    class="col-6 col-md-6 col-lg-4 pointer"
+                    @click="setSelectedItem(item)"
+                >
+                    <LazyComponentProductThumbnail :item="item"/>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
 
 <script>
+    import {ref} from "@nuxtjs/composition-api";
+    import * as data  from "../assets/data/db.json";
+
     export default {
-        
+        setup() {
+
+            let selectedItem = ref(data.doctor.products[0]);
+
+            function setSelectedItem(item) {
+                selectedItem.value = item;
+            }
+
+            return {
+                selectedItem,
+                setSelectedItem,
+                messages: data.messages,
+                doctor: data.doctor,
+            }
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 </style>
